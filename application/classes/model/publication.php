579a98987a0d97519fd43a69a78d1aa6ee1bb2a3
shrek82 <37294812@qq.com>
2013-12-13 10:24:05 +0800

    新增刊物分类

--- application/classes/model/publication.php
+++ application/classes/model/publication.php
@@ -1,17 +1,25 @@
 <?php
-class Model_Publication{
-       const TXT_PATH = 'static/upload/publication/txt/';
-       const PDF_PATH = 'static/upload/publication/pdf/';
-       const COVER_PATH = 'static/upload/publication/cover/';
-       static $up_rule = array(
+
+class Model_Publication {
+
+    const TXT_PATH = 'static/upload/publication/txt/';
+    const PDF_PATH = 'static/upload/publication/pdf/';
+    const COVER_PATH = 'static/upload/publication/cover/';
+
+    static $up_rule = array(
         'Upload::valid' => array(),
         'Upload::not_empty' => array(),
-        'Upload::type' => array('Upload::type' => array('pdf','txt','gif','jpg','jpeg','png')),
+        'Upload::type' => array('Upload::type' => array('pdf', 'txt', 'gif', 'jpg', 'jpeg', 'png')),
         'Upload::size' => array('70M')
     );
+    //刊物类型
+    static $pub_type = array(
+        '1001' => '北航学人',
+        '1002' => '校友风采系列篇',
+        '1003' => '新版北航学人',
+        '1004' => '校庆捐赠指南',
+        '1005' => '北航电子简报',
+        '1006' => '北航通讯'
+    );
 
-       //刊物类型
-       static $pub_type=array(
-	   '1001'=>'北航学人'
-       );
 }
